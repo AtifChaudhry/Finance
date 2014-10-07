75e37798160dd36e354858f260f6ba93e0a775b3
Atif Chaudhry <atif.achaudhry@gmail.com>
2014-10-07 16:44:44 -0700

    Update basic.rb

--- basic.rb
+++ basic.rb
@@ -1,7 +1,7 @@
 # A Cash Flow (CF) represented as an array of numbers.
 
-# Present Value (PV) of a CF
-def PV(cf)
+# Present Value (PV) of a CF at a given rate
+def PV(cf, r)
 end
 
 # Internal Rate of Return (IRR) of a CF
