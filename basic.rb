96c5d63e7c6e0f7c6e92cddff72a8c319a497ecb
Atif Chaudhry <atif.achaudhry@gmail.com>
2014-10-07 17:25:23 -0700

    Complete NVP

--- basic.rb
+++ basic.rb
@@ -1,9 +1,22 @@
-# A Cash Flow (CF) represented as an array of numbers.
+# A Cash Flow (CF) is represented as an array of numbers. For example, 
+# the CF for an intial investment of $100, which pays $0 in the first 
+# period, and then pays $150 in the second period will be: [-100, 0, 150]
 
-# Present Value (PV) of a CF at a given rate
-def PV(cf, r)
+# The rate of return (r) is represented as a float, for example the rate
+# of 10% is represented as 0.1
+
+# Net Present Value (npv) of a cashflow (cf) at a given rate (r).
+def npv(r, cf)
+  cf.zip(0..cf.count-1).inject(0) {|a,v| a+v[0]/((1.0+r)**v[1])}
 end
+
+npv(0.1, [-1, 2]).round(2)
+npv(0.1, [-1, 0, 3]).round(2)
+npv(0.1, [-1, 1, 1, 1, 1, 1, 2]).round(2)
+npv(0.1, [-1, 0, 2, 0, 2, 0, 3]).round(2)
 
 # Internal Rate of Return (IRR) of a CF
 def IRR(cf)
 end
+
+
