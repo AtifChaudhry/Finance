b6856a4d266a7411375f8525526b13cc2843202b
Atif Chaudhry <atif.achaudhry@gmail.com>
2014-10-11 03:24:09 -0700

    change def of cash flow

--- basic.rb
+++ basic.rb
@@ -5,16 +5,15 @@
 # The rate of return (r) is represented as a float, for example the rate
 # of 10% is represented as 0.1
 
-# Net Present Value (npv) of a cashflow (cf) with an intial investement (c0)
-# for a given rate of return (r).
-def npv(r, c0, cf)
-  cf.zip(1..cf.count).inject(c0) {|a,v| a+v[0]/((1.0+r)**v[1])}
+# Net Present Value (npv) of a cashflow (cf) for a given rate of return (r).
+def npv(r, cf)
+  cf.zip(0..cf.count).inject(0) {|a,v| a+v[0]/((1.0+r)**v[1])}
 end
 
-npv(0.1, -1, [2]).round(2)
-npv(0.1, -1, [0, 3]).round(2)
-npv(0.1, -1, [1, 1, 1, 1, 1, 2]).round(2)
-npv(0.1, -1, [0, 2, 0, 2, 0, 3]).round(2)
+npv(0.1, [-1, 2]).round(2)
+npv(0.1, [-1, 0, 3]).round(2)
+npv(0.1, [-1, 1, 1, 1, 1, 1, 2]).round(2)
+npv(0.1, [-1, 0, 2, 0, 2, 0, 3]).round(2)
 
 # Internal Rate of Return (IRR) of a CF
 def IRR(cf)
