defd4e213ec45fdf4d196e0f727c4fe6b4f460f4
Atif Chaudhry <atif.achaudhry@gmail.com>
2014-10-21 15:56:33 -0700

    add hw2

--- basic.rb
+++ basic.rb
@@ -5,7 +5,7 @@
 # The rate of return (r) is represented as a float, for example the rate
 # of 10% is represented as 0.1
 
-# Net Present Value (npv) of a cashflow (cf) for a given rate of return (r).
+# Net Present Value (npv) of a cashflow (cf) at a given rate of return (r).
 def npv(r, cf)
   cf.zip(0..cf.count).inject(0) {|a,v| a+v[0]/((1.0+r)**v[1])}
 end
@@ -14,9 +14,4 @@ npv(0.1, [-1, 2]).round(2)
 npv(0.1, [-1, 0, 3]).round(2)
 npv(0.1, [-1, 1, 1, 1, 1, 1, 2]).round(2)
 npv(0.1, [-1, 0, 2, 0, 2, 0, 3]).round(2)
-
-# Internal Rate of Return (IRR) of a CF
-def IRR(cf)
-end
-
 
