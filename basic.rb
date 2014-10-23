04fb7d55e4b63874756b677627ab26d138caf362
Atif Chaudhry <atif.achaudhry@gmail.com>
2014-10-22 18:24:51 -0700

    Update basic.rb

--- basic.rb
+++ basic.rb
@@ -1,17 +1,30 @@
-# A Cash Flow (CF) is represented as an array of numbers. For example, 
-# the CF for an intial investment of $100, which pays $0 in the first 
-# period, and then pays $150 in the second period will be: [-100, 0, 150]
-
-# The rate of return (r) is represented as a float, for example the rate
-# of 10% is represented as 0.1
-
-# Net Present Value (npv) of a cashflow (cf) at a given rate of return (r).
-def npv(r, cf)
-  cf.zip(0..cf.count).inject(0) {|a,v| a+v[0]/((1.0+r)**v[1])}
-end
-
-npv(0.1, [-1, 2]).round(2)
-npv(0.1, [-1, 0, 3]).round(2)
-npv(0.1, [-1, 1, 1, 1, 1, 1, 2]).round(2)
-npv(0.1, [-1, 0, 2, 0, 2, 0, 3]).round(2)
-
+#####################################################################################
+# Finance functions #
+#####################################################################################
+# Basic compounding and discounting
+# c - amount (single)
+# n - number of periods
+# r - interest rate (per period)
+def compound(c, n, r); c * ((1+r)**n); end
+def discount(c, n, r); c / ((1+r)**n); end
+def comp(c,n,r); compound(c,n,r); end
+def disc(c,n,r); discount(c,n,r); end
+raise "Test 1.a: Failed" if comp(1, 1, 1)!=2 or disc(2, 1, 1)!=1
+raise "Test 1.b: Failed" if disc(comp(9, 7, 0.1), 7, 0.1) != 9
+# Computes the PV and FV of a leveled cache flow (annunity).
+# c - payment (fixed per period)
+# n - number of periods
+# r - interest rate (per period)
+def pv(c, n, r); c*(1.0/r)*(1 - (1.0/(1+r)**n)); end
+def fv(c, n, r); comp(pv(c, n, r), n, r); end
+raise "Test 2.a: Failed" if pv(1000, 10, 0.05) != disc(fv(1000, 10, 0.05), 10, 0.05)
+raise "Test 2.b: Failed" if pv(100, 1, 0.06).round(2) != disc(100, 1, 0.06).round(2)
+raise "Test 2.c: Failed" if fv(100, 2, 0.06).round(2) != (comp(100,1,0.06)+comp(100,0,0.06)).round(2)
+# Computes the payment for a leveled cash flow (annuity).
+# pv - present value (of cash flow)
+# n - number of periods
+# r - interest rate (per period)
+def pmt(pv, n, r); (pv*r)/(1.0 - 1/(1.0+r)**n); end
+raise "Test 3.a: Failed" if pmt(100, 1, 0.1).round(2) != 110
+raise "Test 3.b: Failed" if pmt(pv(1000, 10, 0.06), 10, 0.06).round(2) != 1000
+#####################################################################################
