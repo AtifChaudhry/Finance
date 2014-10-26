2f4d6f4ab101a8b636746bf8cf26820822d99ac2
Atif Chaudhry <atif.achaudhry@gmail.com>
2014-10-26 02:20:01 -0700

    cleanup

--- basic.py
+++ basic.py
@@ -3,9 +3,9 @@
 #####################################################################################
 
 # Basic compounding and discounting
-# c - amount (single)
-# n - number of periods
-# r - interest rate (per period)
+#  c - amount (single)
+#  n - number of periods
+#  r - interest rate (per period)
 def compound(c, n, r): return c * ((1+r)**n)
 def discount(c, n, r): return c / ((1+r)**n)
 comp = compound
@@ -14,9 +14,9 @@ assert (comp(1, 1, 1) == 2 and disc(2, 1, 1) == 1), "Test 1.a: Failed"
 assert (disc(comp(9, 7, 0.1), 7, 0.1) == 9),        "Test 1.b: Failed"
 
 # Computes the PV and FV of a leveled cache flow (annunity).
-# c - payment (fixed per period)
-# n - number of periods
-# r - interest rate (per period)
+#  c - payment (fixed per period)
+#  n - number of periods
+#  r - interest rate (per period)
 def pv(c, n, r): return c*(1.0/r)*(1 - (1.0/(1+r)**n))
 def fv(c, n, r): return comp(pv(c, n, r), n, r)
 assert (pv(1000, 10, 0.05) == disc(fv(1000, 10, 0.05), 10, 0.05)),                  "Test 2.a: Failed" 
@@ -24,20 +24,35 @@ assert (round(pv(100, 1, 0.06), 2) == round(disc(100, 1, 0.06), 2)),            
 assert (round(fv(100, 2, 0.06), 2) == round(comp(100,1,0.06)+comp(100,0,0.06), 2)), "Test 2.c: Failed" 
 
 # Computes the payment for a leveled cash flow (annuity).
-# pv - present value (of cash flow)
-# n - number of periods
-# r - interest rate (per period)
+#  pv - present value (of cash flow)
+#  n - number of periods
+#  r - interest rate (per period)
 def pmt(pv, n, r): return (pv*r*(1.0+r)**n)/((1.0+r)**n - 1)
 assert (round(pmt(100, 1, 0.1), 2) == 110),                   "Test 3.a: Failed" 
 assert (round(pmt(pv(1000, 10, 0.06), 10, 0.06), 2) == 1000), "Test 3.b: Failed" 
 # Net Present Value (NPV) of a cash flow (the initial investment is cf[0])
-# cf - cash flow stream (including any initial investment < 0)
-# r  - the discount rate
+#  cf - cash flow stream (including any initial investment < 0)
+#  r  - the discount rate
+# Example:
+#  The CF for an intial investment of $100, which pays $0 in the first
+#  period, and then pays $150 in the second period will be [-100, 0, 150].
 def npv(cf, r):
  pv = 0.0
  for n in range(len(cf)): pv += disc(cf[n], n, r)
  return pv
-assert (round(npv([-100, 110], 0.1),2) == 0), "Test 4.a: Failed"
+assert (round(npv([-100, 110], 0.1), 2) == 0), "Test 4.a: Failed"
+
+import numpy as np
+
+# Internal Rate of Return (IRR)
+#  cf - cash flow stream (including any initial investment < 0)
+def irr(cf):  np.irr(np.array(cf))
+
+# Crossover Rate - The rate at which the NPV of the two 
+# cash flow streams is equal.
+#  cf1 - the first cash flow stream 
+#  cf2 - the second cash flow stream
+def cor(cf1, cf2): np.irr(np.array(cf1) - np.array(cf2))
 
 # Composes int() and round()
 def rnd(x): return int(round(x))
--- ch8.py
+++ ch8.py
@@ -12,3 +12,10 @@ cf  = [-10000, 2000, 2000, 4000, 4000, 5000]
 r = 0.1
 npv = fin.npv(cf, r)
 print "The NPV is $%i, so %slaunch the project." % (npv, "don't " if npv < 0 else "")
+
+# EG 9.7 Consider the two investments: Investment A: (-400, 250, 280) and 
+# Investment B: (-500, 320, 340). What is the crossover rate (i.e. the 
+# rate at which both have the same NPV).
+inv_a = [-400, 250, 280]
+inv_b = [-500, 320, 340]
+print "crossover rate = %.2f" % round(fin.cor(inv_a, inv_b), 2)
